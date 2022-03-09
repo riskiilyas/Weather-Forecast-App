938f92a1a0ebbd94d10394f8b79954855fc23dbf
riskiilyas <riskiilyas03@gmail.com>
2022-03-09 17:45:55 +0700

    Add Splashscreen

--- app/src/main/java/com/keecoding/weatherforecastapp/MainActivity.kt
+++ app/src/main/java/com/keecoding/weatherforecastapp/MainActivity.kt
@@ -3,13 +3,17 @@ package com.keecoding.weatherforecastapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.keecoding.weatherforecastapp.navigation.WeatherNavigation
 import com.keecoding.weatherforecastapp.ui.theme.WeatherForecastAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,28 +22,30 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WeatherForecastAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            WeatherApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun WeatherApp() {
+    WeatherForecastAppTheme {
+        Surface(color = MaterialTheme.colors.background,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                WeatherNavigation()
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     WeatherForecastAppTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/keecoding/weatherforecastapp/screens/WeatherSplashScreen.kt
+++ app/src/main/java/com/keecoding/weatherforecastapp/screens/WeatherSplashScreen.kt
@@ -1,11 +1,52 @@
 package com.keecoding.weatherforecastapp.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.keecoding.weatherforecastapp.R
 import com.keecoding.weatherforecastapp.navigation.WeatherScreens
 
 @Composable
 fun WeatherSplashScreen(navController: NavController) {
-    Text(text = "SplashScreen")
+    androidx.compose.material.Surface(
+        modifier = Modifier
+            .padding(15.dp)
+            .size(330.dp),
+        shape = CircleShape,
+        color = Color.White,
+        border = BorderStroke(
+            2.dp, Color.Blue
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(1.dp),
+            horizontalAlignment = CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(painter = painterResource(id = R.drawable.splashscreen),
+                contentDescription = "",
+                modifier = Modifier.width(100.dp))
+            Text(text = "How's the weather!",
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.h5
+            )
+
+        }
+    }
 }
