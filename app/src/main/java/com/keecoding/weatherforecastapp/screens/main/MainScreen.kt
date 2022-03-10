1e95720d83a5f2f0988eef7d03049048755d9062
riskiilyas <riskiilyas03@gmail.com>
2022-03-10 09:14:32 +0700

    App Bar in Progress

--- app/src/main/java/com/keecoding/weatherforecastapp/screens/main/MainScreen.kt
+++ app/src/main/java/com/keecoding/weatherforecastapp/screens/main/MainScreen.kt
@@ -1,32 +1,47 @@
 package com.keecoding.weatherforecastapp.screens.main
 
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.navigation.NavController
 import com.keecoding.weatherforecastapp.data.DataOrException
 import com.keecoding.weatherforecastapp.model.Weather
-import com.keecoding.weatherforecastapp.navigation.WeatherScreens
 
 @Composable
 fun MainScreen(
     navController: NavController,
     mainViewModel: MainViewModel
 ) {
-    ShowData(mainViewModel)
-}
-
-@Composable
-fun ShowData(vm: MainViewModel) {
     val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
         initialValue = DataOrException(loading = true)) {
-        value = vm.getWeather("Seattle")
+        value = mainViewModel.getWeather("Seattle")
     }.value
 
     if (weatherData.loading == true) {
         CircularProgressIndicator()
     } else if (weatherData.data != null) {
-        Text(text = "Main Screen ${weatherData.data!!.city.country}")
+        MainScaffold(weatherData.data!!, navController)
     }
+}
+
+@Composable
+fun MainScaffold(weather: Weather, navController: NavController) {
+    
+    Scaffold(
+        topBar = {
+
+        }
+    ) {
+        MainContent(data = weather)
+    }
+
+}
+
+@Composable
+fun MainContent(data: Weather) {
+    
+    Text(text = data.city.name)
+    
 }
---
+++ app/src/main/java/com/keecoding/weatherforecastapp/widgets/WeatherAppBar.kt
@@ -0,0 +1,12 @@
+package com.keecoding.weatherforecastapp.widgets
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+
+@Composable
+fun WeatherAppBar(
+    title: String = "Title",
+    icon: ImageVector? = null
+) {
+
+}
